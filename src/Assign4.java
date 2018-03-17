9dc44a1bf944982c75879842a3006b8b63d11634
Jonathan Yee <webber989@gmail.com>
2018-03-16 18:22:38 -0600

    Added empty classes for graphs and one input file for and adjacency input

--- src/Assign4.java
+++ src/Assign4.java
@@ -15,16 +15,17 @@ public class Assign4 {
 	public static void main(String[] args) {
 		//Command line argument verification 
 		
-				if(args.length != 4) {
-					System.out.println("Incorrect number of inputs. Quitting...");
-					System.exit(-1);
-				}
-				if(!getFileExtension(args[0]).equals(".txt") || !getFileExtension(args[1]).equals(".txt") || !getFileExtension(args[2]).equals(".txt")
-						|| !getFileExtension(args[3]).equals(".txt") ) {
-					System.out.println("Unable to use files that are not text files. Check your file names. Quitting...");
-					System.exit(-1);
-				}
-
+		if(args.length != 4) {
+			System.out.println("Incorrect number of inputs. Quitting...");
+			System.exit(-1);
+		}
+		if(!getFileExtension(args[0]).equals(".txt") || !getFileExtension(args[1]).equals(".txt") || !getFileExtension(args[2]).equals(".txt")
+				|| !getFileExtension(args[3]).equals(".txt") ) {
+			System.out.println("Unable to use files that are not text files. Check your file names. Quitting...");
+			System.exit(-1);
+		}
+		//Build the adjacency matrix from an input file
+				
 	}
 
 }
